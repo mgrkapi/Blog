4b7633620c55b7512d04318c5881a2960126abaf
mgrkapi <mgrkapi@wp.pl>
2022-08-16 18:45:15 +0200

    Added links to pages and basic styles to navigation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,12 @@ import Login from "./pages/Login";
 function App() {
     return (
         <Router>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/createPost">Post</Link>
+                <Link to="/login">Login</Link>
+
+            </nav>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/createpost" element={<CreatePost/>}/>
