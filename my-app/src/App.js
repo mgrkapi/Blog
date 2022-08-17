ea26ba0c8a1ca88892d980282159785c559bacc1
mgrkapi <mgrkapi@wp.pl>
2022-08-17 13:32:52 +0200

    Added log in and log out functionalities

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,20 +3,35 @@ import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Login from "./pages/Login";
+import {useState} from "react";
+import {signOut} from "firebase/auth";
+import {auth} from "./firebase";
 
 function App() {
+    //function that says if the user is logged in or not
+    const[isAuth, setIsAuth] = useState(false);
+
+
+    const signUserOut = () => {
+        signOut(auth).then(() => {
+            localStorage.clear();
+            setIsAuth(false);
+            window.location.pathname = "/login";
+        })
+    }
     return (
         <Router>
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/createPost">Post</Link>
-                <Link to="/login">Login</Link>
-
+                {/*//if user is logged in, don't show login lin, just show log out button*/}
+                {!isAuth ? <Link to="/login">Login</Link> : <button onClick={signUserOut}>Log Out</button>}
             </nav>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/createpost" element={<CreatePost/>}/>
-                <Route path="/login" element={<Login/>}/>
+                {/*// passing a state as a prop to the login component*/}
+                <Route path="/login" element={<Login setIsAuth = {setIsAuth}/>}/>
             </Routes>
         </Router>
     )
--- my-app/src/pages/Login.js
+++ my-app/src/pages/Login.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import {auth, provider} from '../firebase';
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
-    return <div>Login</div>;
+function Login({setIsAuth}) {
+
+    let navigate = useNavigate();
+
+    const signInWithGoogle = () => {
+        //result is holding information about the logged in user
+        signInWithPopup(auth, provider).then((result) => {
+            //identifier in the local storage to determine if we are logged in or not
+            localStorage.setItem("isAuth", true);
+            //saying that user is logged in
+            setIsAuth(true)
+            navigate("/"); //redirect to home page when logged in
+        })
+    }
+
+    return <div className="login-page">
+        <p>Sign in with Google</p>
+        <button className="login-with-google-btn" onClick={signInWithGoogle}>
+            Sign in with Google
+        </button>
+    </div>;
 }
 
 export default Login;
