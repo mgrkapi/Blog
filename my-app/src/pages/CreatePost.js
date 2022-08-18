e4bfb00e9efdfcb1a8c18d621b1efd0d98d608e1
mgrkapi <mgrkapi@wp.pl>
2022-08-18 17:02:53 +0200

    Created basic post structure

--- my-app/src/pages/CreatePost.js
+++ my-app/src/pages/CreatePost.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function CreatePost() {
-    return <div>Post</div>;
+    return <div className = "createPostPage">
+        <div className = "createPostContainer">
+            <h1> Create Post</h1>
+            <div className = "post">
+                <label>Title</label>
+                <input placeholder = "Title..."/>
+            </div>
+            <div className = "post">
+                <label>Post:</label>
+                <textarea placeholder = "Post..."/>
+            </div>
+        </div>
+    </div>;
 }
 
 export default CreatePost;
