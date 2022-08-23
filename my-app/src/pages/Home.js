110974bfdda17481465c3de26918398a63a3b287
mgrkapi <mgrkapi@wp.pl>
2022-08-23 15:24:45 +0200

    Created display of posts at the home page

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {collection, getDocs} from 'firebase/firestore';
+import {db} from "../firebase";
 
 function Home() {
-    return <div>Home</div>
+    const [postLists, setPostList] = useState([]);
+    const postsCollectionRef = collection(db, "posts");
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await getDocs(postsCollectionRef);
+            setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+        };
+        getPosts();
+    });
+    return <div className="homePage">{postLists.map((post) =>{
+        return <div className="post">
+            <div className="postHeader">
+                <div className="title"><h1>{post.title}</h1>
+                </div>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <h3>@{post.author.name}</h3>
+        </div>
+    })}</div>
 }
 
 export default Home;
