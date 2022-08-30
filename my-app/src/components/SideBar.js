6d6a10b1b85d1ce35d09aa5865e75203b15e5beb
mgrkapi <mgrkapi@wp.pl>
2022-08-30 16:15:24 +0200

    Added sidebar and basic styles

---
+++ my-app/src/components/SideBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function SideBar() {
+    return (
+        <div className="side-bar">
+            <div className="side-bar__content">
+                <img src=""/>
+                <div className="side-bar__header">Welcome to my blog</div>
+                <div className="side-bar__title">This is my first blog</div>
+                <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Lorem ipsum dividend adip tempor
+                </article>
+            </div>
+        </div>
+
+    )
+}
+
+export default SideBar;
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -3,6 +3,7 @@ import {collection, getDocs, deleteDoc, doc} from 'firebase/firestore';
 import {auth, db, storage} from "../firebase";
 import {getDownloadURL, listAll, ref} from "firebase/storage";
 import "../style/home.scss";
+import SideBar from "../components/SideBar";
 
 function Home({isAuth}) {
     const [postLists, setPostList] = useState([]);
@@ -33,30 +34,37 @@ function Home({isAuth}) {
     }, []);
 
 
-    return <div className="homePage">{postLists.map((post) => {
-        return <div className="post" key={post.id}>
-            <div className="postHeader">
-                <div className="title">
-                    <h1>{post.title}</h1>
-                </div>
-                <div className="deletePost">
-                    {isAuth && post.author.id === auth.currentUser.uid && (
-                    <button
-                        onClick={() => {
-                        deletePost(post.id);
-                    }}>
-                        &#128465;
-                </button>
-                )}
+    return (
+        <>
+            <div className="home-container">
+                <SideBar className="sidebar"/>
+                <div className="homePage">{postLists.map((post) => {
+                    return <div className="post" key={post.id}>
+                        <div className="postHeader">
+                            <div className="title">
+                                <h1>{post.title}</h1>
+                            </div>
+                            <div className="deletePost">
+                                {isAuth && post.author.id === auth.currentUser.uid && (
+                                    <button
+                                        onClick={() => {
+                                            deletePost(post.id);
+                                        }}>
+                                        &#128465;
+                                    </button>
+                                )}
+                            </div>
+                        </div>
+                        <div className="postTextContainer">{post.postText}</div>
+                        {imageList.map((url, id) => {
+                            return <img src={url} key={id}/>
+                        })}
+                        <h3>@{post.author.name}</h3>
+                    </div>
+                })}</div>
             </div>
-        </div>
-        <div className="postTextContainer">{post.postText}</div>
-            {imageList.map((url, id) => {
-            return <img src = {url} key = {id}/>
-            })}
-        <h3>@{post.author.name}</h3>
-    </div>
-    })}</div>
+        </>
+    )
 }
 
 export default Home;
