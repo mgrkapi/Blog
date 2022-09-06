10dac8b75f8f8c1566ef56b6564c43cfc42033e1
mgrkapi <mgrkapi@wp.pl>
2022-09-06 10:46:10 +0200

    Modified sidebar and added basic styles

--- my-app/src/components/SideBar.js
+++ my-app/src/components/SideBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import profile from "../pictures/profile.jpg";
+import "../style/sidebar.scss";
 
 function SideBar() {
     return (
         <div className="side-bar">
             <div className="side-bar__content">
-                <img src=""/>
+                <img src={profile} alt="woman face with lake in the background"/>
                 <div className="side-bar__header">Welcome to my blog</div>
-                <div className="side-bar__title">This is my first blog</div>
                 <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
                     consectetur adipiscing elit. Lorem ipsum dividend adip tempor
                 </article>
